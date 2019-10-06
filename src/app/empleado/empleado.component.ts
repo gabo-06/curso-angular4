6d2ecdda77fc46fb31e2bf586bc27d903c20fa85
gabo-06 <gabrielangelgf@hotmail.com>
2019-10-06 04:34:00 +0000

    Probe subir para verificar que el commit funciona desde que hago un cambio en mi PC local pasando por el ftp

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -35,3 +35,4 @@ export class EmpleadoComponent implements OnInit
         this.trabajador_externo = valor;
     }
 }
+
--- src/app/empleado/empleado.ts
+++ src/app/empleado/empleado.ts
@@ -23,3 +23,6 @@ export class Empleado
         public contratado: boolean
     ){}
 }
+
+
+// marujaaaaaaaaaaaaaaaaaaaa
